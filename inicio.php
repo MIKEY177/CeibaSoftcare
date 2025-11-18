c73f135f58b1dbe8a18366d4363a1fd316b5c9c8
Juesrogi2007 <rodriguezgiljuanesteban7@gmail.com>
2025-11-18 17:56:25 -0500

    Creación de archivo inicio: -Se crea el archivo inicio.php.

---
+++ inicio.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/main_style.css">
+    <title>Inicio - Softcare</title>
+</head>
+<body>
+    <main>
+        <aside class="vertical-navbar">
+            <div class="perfil">
+                <figure class="avatar">
+                    <img class="avatar-img" src="" alt=""> <!-- Variable -->
+                </figure>
+                <h1 class="perfil-nombre">[Username...]</h1> <!-- Variable -->
+                <figure class="perfil-rol">
+                    <img class="perfil-rol-img" src="" alt="">
+                    <h1 class="perfil-rol-texto">[Rol]</h1> <!-- Variable -->
+                </figure>
+            </div>
+            <div class="contenedor-nav">
+                <a href=""><h2 class="opcion-nav">Inicio</h2></a>
+                <a href=""><h2 class="opcion-nav">Farmacia</h2></a>
+                <a href=""><h2 class="opcion-nav">Refugio</h2></a>
+            </div>
+            <a href=""><button class="cerrar-sesion-btn">Cerrar Sesión</button></a>
+        </aside>
+        <section class="secciones-dashboard">
+            <h2 class="titulo-dashboard">¡Bienvenido al Dashboard!</h2>
+            <section class="seccion1-proximas-brigadas">
+                <h3 class="subtitulo-dashboard">Próximas Brigadas</h3>
+                <section class="area-brigadas">
+                    <div class="subarea-brigada"> <!-- Este div es el que se repite en Backend -->
+                        <h4 class="fecha">08 de Abril del 2025</h4> <!-- Variable -->
+                        <article class="articulo-brigada">
+                            <h5 class="detalles-brigada">[Nombre brigada]</h5> <!-- Variable -->
+                            <h6 class="detalles-brigada">Lugar:</h6>
+                            <p class="detalles-brigada">[contenido]</p> <!-- Variable -->
+                            <h6 class="detalles-brigada">Descrpción:</h6>
+                            <p class="detalles-brigada">[contenido]</p> <!-- Variable -->
+                        </article>
+                    </div>
+                    <div class="separador-vertical"></div> <!-- Este div también se repite -->
+                </section>
+            </section>
+            <section class="seccion2-modulos">
+                <h3 class="subtitulo-dashboard">Módulos de Gestión</h3>
+                <section class="area-modulos">
+                    <a href="">
+                        <div class="modulo-farmacia">
+                            <h4 class="titulo-modulo-farmacia">Farmacia</h4>
+                            <figure class="modulo-farmacia-icono">
+                                <img class="modulo-farmacia-img" src="" alt="">
+                            </figure>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="modulo-refugio">
+                            <h4 class="titulo-modulo-refugio">Refugio</h4>
+                            <figure class="modulo-refugio-icono">
+                                <img class="modulo-refugio-img" src="" alt="">
+                            </figure>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="modulo-usuarios">
+                            <h4 class="titulo-modulo-usuarios">Usuarios</h4>
+                            <figure class="modulo-usuarios-icono">
+                                <img class="modulo-usuarios-img" src="" alt="">
+                            </figure>
+                        </div>
+                    </a>   
+                </section>
+            </section>
+        </section>
+    </main>
+    <footer>
+        <p>Derechos Reservados(menos a GatitoFeroz).</p> <!-- No sé que va acá exactamente -->
+        <section class="asociaciones">
+            <div class="softcare">
+                <figure class="softcare-logo">
+                    <img class="softcare-logo-img" src="" alt="">
+                </figure>
+                <h6 class="softcare-titulo">SoftCare</h6>
+            </div>
+            <div class="ceiba">
+                <figure class="ceiba-logo">
+                    <img class="ceiba-logo-img" src="" alt="">
+                </figure>
+                <h6 class="ceiba-titulo">Ceiba</h6>
+            </div>
+            <!-- <div class="sena">
+                <figure class="sena-logo">
+                    <img class="sena-logo-img" src="" alt="">
+                </figure>
+                <h6 class="sena-titulo">Sena</h6>
+            </div> -->
+        </section>
+    </footer>
+</body>
+</html>
