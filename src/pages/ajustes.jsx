95f6df58fde00d8457fe6a4182ea283a91bae639
Miguel Hincapie Agudelo <itsjustmike177@gmail.com>
2026-05-03 13:01:24 -0500

    Creación y estilización ajustes.jsx #2 - Se crean las modales de ajustes.jsx - Se realizan cambios a la tarjeta de ajustes.jsx - Se cambia la imágen card_background.png - Se agrega la imágen subir.png para el input type=file en la su modal respectiva

--- src/pages/ajustes.jsx
+++ src/pages/ajustes.jsx
@@ -7,6 +7,7 @@ import { MenuAdmin, MenuAdminFarmacia, MenuAdminAlbergue, MenuFarmaceutico, Menu
 import "../styles/global_styles.css"
 import "../styles/ajustes.css"
 import editarIcon from "../images/icons/editar.png"
+import subirIcon from "../images/subir.png"
 import campoRestringido from "../images/candado.png"
 import flecha from "../images/flecha_salir.png"
 import pfpPlaceholder from "../../images/pfp_placeholder.png"
@@ -31,102 +32,195 @@ export const Ajustes = () => {
         <section className="secciones-area-gestion">
           <h2 className="titulo-dashboard">Ajustes e Información del Usuario</h2>
           <section className='seccion1-ajustes'>
-            <button className="editar-ajustes" onClick={''}>
-              <img className="editar-ajustes-img" src={editarIcon} alt="" />
-            </button>
             <label className='user-info-avatar' style={{ gridArea: "avatar" }}>
               <figure className="avatar-ajustes" style={{ width: "220px", height: "220px" }}>
                 <img className="avatar-img" src={pfpPlaceholder} alt="" />
               </figure>
               <h4>Imágen de Perfil</h4>
+              <figure className="editar-ajustes" onClick={''}>
+                <img className="editar-ajustes-img" src={editarIcon} alt="" />
+              </figure>
             </label>
             <label className='user-info' style={{ gridArea: "nombre" }}>
               <h3>Nombre:&nbsp;</h3>
               <h4>[Nombre_del_Usuario].</h4>
+              <figure className="editar-icono-ajus" onClick={''}>
+                <img className="editar-icono-img" src={editarIcon} alt="Editar" />
+              </figure>
             </label>
             <label className='user-info' style={{ gridArea: "correo" }}>
               <h3>Correo:&nbsp;</h3>
               <h4>[Correo_del_Usuario].</h4>
+              <figure className="editar-icono-ajus" onClick={''}>
+                <img className="editar-icono-img" src={editarIcon} alt="Editar" />
+              </figure>
             </label>
             <label className='user-info' style={{ gridArea: "contrasena" }}>
               <h3>Contraseña:&nbsp;</h3>
               <h4>[Contraseña_del_Usuario].</h4>
+              <figure className="editar-icono-ajus" onClick={''}>
+                <img className="editar-icono-img" src={editarIcon} alt="Editar" />
+              </figure>
             </label>
             <label className='user-info' style={{ gridArea: "rol" }}>
               <h3>Rol Actual:&nbsp;</h3>
               <h4>[Rol_del_Usuario].</h4>
+              {/* <figure className="editar-icono-ajus" onClick={''}>
+                <img className="editar-icono-img" src={editarIcon} alt="Editar" />
+              </figure> */}
+              <figure className="candado-icono-ajus">
+                <img className="candado-icono-img" src={campoRestringido} alt="" />
+              </figure>
             </label>
           </section>
         </section>
       </main>
       <Footer />
-      
+
       <div className="modales-ajustes" style={{}}>
-        <aside className="modal-ajustes-editar">
+
+        {/* modal #1 editar Nombres */}
+
+        <aside className="modal-ajustes-editar1">
           <button className="volver-btn-ajus" onClick={''}>
             <img className="volver-icono" src={flecha} alt="" />
             <h2>Volver</h2>
           </button>
           <h1 className="modal-ajustes-titulo">
-            Editar Información del Usuario
+            Editar Nombres
           </h1>
 
           {/* {mensajeExito    && <p style={{ color: "green", fontWeight: "bold" }}>{mensajeExito}</p>}
                     {errores.general && <p style={{ color: "red" }}>{errores.general}</p>} */}
 
           <form className="ajustes-form" onSubmit={''}>
-            <section className="ajustes-form-inputs-area">
+            <section className="ajustes-form-inputs-area1">
 
               <div style={{ gridArea: "divInpt1" }}>
-                <label className="ajustes-label" for="">Imágen de perfil</label>
-                <input className="ajustes-input1" type="file" accept="image/*" value={''} onChange={''} />
+                <label className="ajustes-label" for="">Nombres y Apellidos<h6 className="obligatorio">*</h6></label>
+                <input className="ajustes-input1" type="text" onChange={''} />
                 {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
-              </div>
-
-              <div style={{ gridArea: "divInpt2" }}>
-                <label className="ajustes-label" for="">Nombre<h6 className="obligatorio">*</h6></label>
-                <input className="ajustes-input2" type="text" value={'[Tipo]'} onChange={''} />
-                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
-              </div>
-
-              <div style={{ gridArea: "divInpt3" }}>
-                <label className="ajustes-label" for="">Correo<h6 className="obligatorio">*</h6></label>
-                <input className="ajustes-input3" type="text" value={'[Nombre_del_Ajuste]'} onChange={''} />
-                {/* <span className="error-mensaje">{errores.descripcion ?? ""}</span> */}
-              </div>
-
-              <div style={{ gridArea: "divInpt4" }}>
-                <label className="ajustes-label" for="">Especie<h6 className="obligatorio">*</h6></label>
-                <input className="ajustes-input4" type="text" value={'[Especie]'} onChange={''} />
-                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
-              </div>
-
-              <div style={{ gridArea: "divInpt5" }}>
-                <label className="ajustes-label" for="">Sexo<h6 className="obligatorio">*</h6></label>
-                <input className="ajustes-input5" type="text" value={'[Sexo]'} onChange={''} />
-                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
-              </div>
-
-              <div style={{ gridArea: "divInpt6" }}>
-                <label className="ajustes-label" for="">Edad Estimada<h6 className="obligatorio">*</h6></label>
-                <input className="ajustes-input6" type="text" value={'[Edad_Estimada]'} onChange={''} />
-                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
-              </div>
-
-              <div className="label-and-input-container" style={{ gridArea: "divInpt7" }}>
-                <label className="ajustes-label" for="">Usuario que Registra</label>
-                <div className="union-input-icono">
-                  <input className="ajustes-input7" type="text" value={'[Usuario_que_Registra]'} readOnly />
-                  <figure className="candado-icono">
-                    <img className="candado-icono-img" src={campoRestringido} alt="" />
-                  </figure>
-                </div>
               </div>
 
             </section>
             <input className="ajustes-btn" type="submit" value={''} disabled={''} />
           </form>
         </aside>
+
+        {/* modal #2 editar Correo */}
+        <aside className="modal-ajustes-editar2">
+          <button className="volver-btn-ajus" onClick={''}>
+            <img className="volver-icono" src={flecha} alt="" />
+            <h2>Volver</h2>
+          </button>
+          <h1 className="modal-ajustes-titulo">
+            Editar Correo
+          </h1>
+
+          <form className="ajustes-form" onSubmit={''}>
+            <section className="ajustes-form-inputs-area2">
+
+              <div style={{ gridArea: "divInpt1" }}>
+                <label className="ajustes-label" for="">Correo Actual<h6 className="obligatorio">*</h6></label>
+                <input className="ajustes-input2" type="text" onChange={''} />
+                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
+              </div>
+
+              <div style={{ gridArea: "divInpt2" }}>
+                <label className="ajustes-label" for="">Nuevo Correo<h6 className="obligatorio">*</h6></label>
+                <input className="ajustes-input2" type="text" onChange={''} />
+                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
+              </div>
+
+            </section>
+            <input className="ajustes-btn" type="submit" value={''} disabled={''} />
+          </form>
+        </aside>
+
+        {/* modal #3 editar Contraseña */}
+        <aside className="modal-ajustes-editar3">
+          <button className="volver-btn-ajus" onClick={''}>
+            <img className="volver-icono" src={flecha} alt="" />
+            <h2>Volver</h2>
+          </button>
+          <h1 className="modal-ajustes-titulo">
+            Editar Contraseña
+          </h1>
+
+          <form className="ajustes-form" onSubmit={''}>
+            <section className="ajustes-form-inputs-area3">
+
+              <div style={{ gridArea: "divInpt1" }}>
+                <label className="ajustes-label" for="">Contraseña Actual<h6 className="obligatorio">*</h6></label>
+                <input className="ajustes-input3" type="password" onChange={''} />
+                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
+              </div>
+
+              <div style={{ gridArea: "divInpt2" }}>
+                <label className="ajustes-label" for="">Nueva Contraseña<h6 className="obligatorio">*</h6></label>
+                <input className="ajustes-input3" type="password" onChange={''} />
+                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
+              </div>
+
+              <div style={{ gridArea: "divInpt3" }}>
+                <label className="ajustes-label" for="">Confirmar Contraseña Nueva<h6 className="obligatorio">*</h6></label>
+                <input className="ajustes-input3" type="password" onChange={''} />
+                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
+              </div>
+
+            </section>
+            <input className="ajustes-btn" type="submit" value={''} disabled={''} />
+          </form>
+        </aside>
+
+        {/* modal #4 editar Rol */}
+        {/* <aside className="modal-ajustes-editar4">
+          <button className="volver-btn-ajus" onClick={''}>
+            <img className="volver-icono" src={flecha} alt="" />
+            <h2>Volver</h2>
+          </button>
+          <h1 className="modal-ajustes-titulo">
+            Editar Rol del Usuario
+          </h1>
+
+
+        </aside> */}
+
+        {/* modal #5 editar Correo */}
+        <aside className="modal-ajustes-editar5">
+          <button className="volver-btn-ajus" onClick={''}>
+            <img className="volver-icono" src={flecha} alt="" />
+            <h2>Volver</h2>
+          </button>
+          <h1 className="modal-ajustes-titulo">
+            Editar Imágen de Perfil
+          </h1>
+
+          <form className="ajustes-form-img-file" onSubmit={''}>
+            <section className="ajustes-form-inputs-area5">
+
+              <div style={{ gridArea: "divInpt1" }}>
+                <label className="ajustes-label" for="">Subir Foto de Perfil<h6 className="obligatorio">*</h6></label>
+                <label className="ajustes-input5">
+                  <label className="ajustes-input5-form" for="file">
+                    <figure className="subir-icono">
+                      <img className="subir-icono-img" src={subirIcon} alt="Editar" />
+                    </figure>
+                    <h5>Seleccionar Archivo...</h5>
+                    <input className="ajustes-input5-file-input" type="file" accept="image/*" id="file" value={''} onChange={''} />
+                  </label>
+                </label>
+                
+                {/* <span className="error-mensaje">{errores.nombre ?? ""}</span> */}
+              </div>
+
+            </section>
+            <input className="ajustes-btn" type="submit" value={''} disabled={''} />
+          </form>
+        </aside>
+
+
+
       </div>
     </>
   )
